24e6af7389bab96915588ca28a674d0f59c1e917
Admin <shade641114@gmail.com>
2024-06-11 23:23:43 +0300

    Editing_code 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,24 +20,28 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.22.0")
-    testImplementation("org.mockito:mockito-core:5.10.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.10.0")
+    implementation ("com.konghq:unirest-java:3.13.6")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation ("org.apache.commons:commons-text:1.10.0")
+    implementation("org.jsoup:jsoup:1.17.2")
+    implementation("gg.jte:jte:3.1.10")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 
-    implementation("io.javalin:javalin:6.1.3")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("org.postgresql:postgresql:42.7.2")
+
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("io.javalin:javalin-testtools:6.1.3")
 
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("com.h2database:h2:2.2.224")
-    implementation("org.postgresql:postgresql:42.7.1")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("gg.jte:jte:3.1.10")
-    implementation("commons-validator:commons-validator:1.8.0")
-    implementation("com.konghq:unirest-java:3.13.6")
-    implementation("org.jsoup:jsoup:1.17.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation ("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation ("org.assertj:assertj-core:3.24.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.test {
